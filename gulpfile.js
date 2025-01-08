96e95a3cb2849c5007ecd4d90e79e5bdd4a43813
rezmes <rezmes@gmail.com>
2025-01-08 16:03:16 +0330

    crypto problem in browser

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,35 @@
-'use strict';
+"use strict";
 
-const build = require('@microsoft/sp-build-web');
+// const build = require('@microsoft/sp-build-web');
 
-build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
+// build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
-build.initialize(require('gulp'));
+// build.initialize(require('gulp'));
+"use strict";
+
+const build = require("@microsoft/sp-build-web");
+const webpack = require("webpack");
+
+build.configureWebpack.mergeConfig({
+  additionalConfiguration: (generatedConfiguration) => {
+    generatedConfiguration.resolve.fallback = {
+      crypto: require.resolve("crypto-browserify"),
+      buffer: require.resolve("buffer"),
+      stream: require.resolve("stream-browserify"),
+      assert: require.resolve("assert"),
+      util: require.resolve("util"),
+      process: require.resolve("process/browser"),
+    };
+
+    generatedConfiguration.plugins.push(
+      new webpack.ProvidePlugin({
+        Buffer: ["buffer", "Buffer"],
+        process: "process/browser",
+      })
+    );
+
+    return generatedConfiguration;
+  },
+});
+
+build.initialize(require("gulp"));
