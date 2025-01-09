df747b0800691d4974e6136ebd2d3198bea27096
rezmes <rezmes@gmail.com>
2025-01-09 18:22:44 +0330

    Load Employee in dropdown

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,8 @@
-'use strict';
-
-<<<<<<< HEAD
+"use strict";
 const build = require("@microsoft/sp-build-web");
-const webpack = require("webpack");
 
-build.configureWebpack.mergeConfig({
-  additionalConfiguration: (generatedConfiguration) => {
-    generatedConfiguration.resolve.fallback = {
-      crypto: require.resolve("crypto-browserify"),
-      buffer: require.resolve("buffer"),
-      stream: require.resolve("stream-browserify"),
-      assert: require.resolve("assert"),
-      util: require.resolve("util"),
-      process: require.resolve("process/browser"),
-    };
-
-    generatedConfiguration.plugins.push(
-      new webpack.ProvidePlugin({
-        Buffer: ["buffer", "Buffer"],
-        process: "process/browser",
-      })
-    );
-
-    return generatedConfiguration;
-  },
-});
+build.addSuppression(
+  `Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`
+);
 
 build.initialize(require("gulp"));
-=======
-const build = require('@microsoft/sp-build-web');
-
-build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
-
-build.initialize(require('gulp'));
->>>>>>> parent of 96e95a3 (crypto problem in browser)
