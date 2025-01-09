dcd4ffdb09845d94aa37a9d24d340e58795f4045
rezmes <rezmes@gmail.com>
2025-01-09 14:26:20 +0330

    New change to NoPnP

--- src/webparts/personnelAppraisal/components/EvaluationPeriod.tsx
+++ src/webparts/personnelAppraisal/components/EvaluationPeriod.tsx
@@ -1,6 +1,86 @@
+// // import * as React from "react";
+// // import { sp } from "@pnp/sp/presets/all";
+// // import "./PersonnelAppraisal.module.scss"; // Import your styles
+
+// // interface IEvaluationPeriodState {
+// //   evaluationPeriod: string;
+// //   isLoading: boolean;
+// //   errorMessage: string | null;
+// // }
+
+// // interface IEvaluationPeriodProps {
+// //   spfxContext: any;
+// //   onPeriodLoaded: (period: string) => void;
+// // }
+
+// // class EvaluationPeriod extends React.Component<
+// //   IEvaluationPeriodProps,
+// //   IEvaluationPeriodState
+// // > {
+// //   constructor(props: IEvaluationPeriodProps) {
+// //     super(props);
+// //     this.state = {
+// //       evaluationPeriod: "",
+// //       isLoading: false,
+// //       errorMessage: null,
+// //     };
+// //   }
+
+// //   componentDidMount(): void {
+// //     sp.setup({
+// //       spfxContext: this.props.spfxContext,
+// //     });
+// //     this.loadLatestPeriod();
+// //   }
+
+// //   private async loadLatestPeriod(): Promise<void> {
+// //     try {
+// //       this.setState({ isLoading: true });
+
+// //       const items = await sp.web.lists
+// //         .getByTitle("EvaluationPeriod")
+// //         .items.orderBy("Created", false)
+// //         .top(1)
+// //         .get();
+
+// //       if (items.length > 0) {
+// //         const evaluationPeriod = items[0].Title;
+// //         this.setState({ evaluationPeriod, isLoading: false });
+// //         this.props.onPeriodLoaded(evaluationPeriod);
+// //       } else {
+// //         this.setState({
+// //           errorMessage: "دوره ارزیابی یافت نشد.",
+// //           isLoading: false,
+// //         });
+// //       }
+// //     } catch (error) {
+// //       this.setState({
+// //         errorMessage: "خطا در بارگذاری دوره ارزیابی",
+// //         isLoading: false,
+// //       });
+// //       console.error("بارگذاری این دوره با خطا مواجه شد: ", error);
+// //     }
+// //   }
+
+// //   render(): React.ReactElement<any> {
+// //     const { evaluationPeriod, isLoading, errorMessage } = this.state;
+
+// //     if (isLoading) {
+// //       return <div>بارگذاری دوره ی ارزیابی ...</div>;
+// //     }
+
+// //     if (errorMessage) {
+// //       return <div style={{ color: "red" }}>{errorMessage}</div>;
+// //     }
+
+// //     return <div id="PeriodTitle">دوره ارزیابی جاری: {evaluationPeriod}</div>;
+// //   }
+// // }
+
+// // export default EvaluationPeriod;
+
 // import * as React from "react";
 // import { sp } from "@pnp/sp/presets/all";
-// import "./PersonnelAppraisal.module.scss"; // Import your styles
 
 // interface IEvaluationPeriodState {
 //   evaluationPeriod: string;
@@ -73,14 +153,16 @@
 //       return <div style={{ color: "red" }}>{errorMessage}</div>;
 //     }
 
-//     return <div id="PeriodTitle">دوره ارزیابی جاری: {evaluationPeriod}</div>;
+//     return (
+//       <div style={{ textAlign: "left", fontFamily: "IRANSansXFaNum" }}>
+//         دوره ارزیابی جاری: {evaluationPeriod}
+//       </div>
+//     );
 //   }
 // }
 
 // export default EvaluationPeriod;
-
 import * as React from "react";
-import { sp } from "@pnp/sp/presets/all";
 
 interface IEvaluationPeriodState {
   evaluationPeriod: string;
@@ -107,9 +189,6 @@ class EvaluationPeriod extends React.Component<
   }
 
   componentDidMount(): void {
-    sp.setup({
-      spfxContext: this.props.spfxContext,
-    });
     this.loadLatestPeriod();
   }
 
@@ -117,14 +196,19 @@ class EvaluationPeriod extends React.Component<
     try {
       this.setState({ isLoading: true });
 
-      const items = await sp.web.lists
-        .getByTitle("EvaluationPeriod")
-        .items.orderBy("Created", false)
-        .top(1)
-        .get();
+      const response = await fetch(
+        `${this.props.spfxContext.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('EvaluationPeriod')/items?$orderby=Created desc&$top=1`,
+        {
+          headers: {
+            Accept: "application/json;odata=verbose",
+          },
+        }
+      );
 
-      if (items.length > 0) {
-        const evaluationPeriod = items[0].Title;
+      const data = await response.json();
+
+      if (data.d.results.length > 0) {
+        const evaluationPeriod = data.d.results[0].Title;
         this.setState({ evaluationPeriod, isLoading: false });
         this.props.onPeriodLoaded(evaluationPeriod);
       } else {
@@ -153,11 +237,7 @@ class EvaluationPeriod extends React.Component<
       return <div style={{ color: "red" }}>{errorMessage}</div>;
     }
 
-    return (
-      <div style={{ textAlign: "left", fontFamily: "IRANSansXFaNum" }}>
-        دوره ارزیابی جاری: {evaluationPeriod}
-      </div>
-    );
+    return <div id="PeriodTitle">دوره ارزیابی جاری: {evaluationPeriod}</div>;
   }
 }
 
